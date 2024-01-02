5fab7f6160aa15e666f880e829d8134a8ee9dba3
keisuke sato <corydoras121@gmail.com>
2024-01-02 10:59:08 +0900

    [update]コメント設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  // モード値を production に設定すると最適化された状態で、
-  // development に設定するとソースマップ有効でJSファイルが出力される
+  // 出力モード
   mode: 'production',
+  // mode: 'development',
   
-  // メインとなるJavaScriptファイル（エントリーポイント）
+  // エントリーポイント
   entry: {
     // エントリーポイントのファイル名をキーにする
     main: './html/assets/js/src/main.js',
@@ -21,8 +21,7 @@ module.exports = {
     filename: './assets/js/dist/[name].bundle.js'
   },
 
-  // ローカル開発用環境を立ち上げる
-  // 実行時にブラウザが自動的に localhost を開く
+  // ローカル開発用環境設定
   devServer: {
     // ルートディレクトリの指定
     contentBase: path.join(__dirname, 'html'),
